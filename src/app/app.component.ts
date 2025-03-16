76d32135608a5dd2612b8d2e739872992f0f08f5
fearles545 <iligol3@gmail.com>
2025-03-16 22:18:11 +0200

    feat: implement main profile component and enhance navbar styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
