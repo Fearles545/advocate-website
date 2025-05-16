d680b869b42340b836a4bdc38b6945345a606e6c
fearles545 <iligol3@gmail.com>
2025-05-16 21:38:00 +0300

    feat: update main component styles and structure, enhance button layout for improved UI

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,10 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-main',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, MatButtonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
 })
