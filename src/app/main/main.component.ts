fefad85e3866a5aea446ecc0f606aad459e30a2c
fearles545 <iligol3@gmail.com>
2025-05-25 12:04:30 +0300

    feat: update styles and structure for main and footer components, enhance responsiveness and layout

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,11 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main',
-  imports: [NgOptimizedImage, MatButtonModule],
+  imports: [NgOptimizedImage, MatButtonModule, RouterLink],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
 })
