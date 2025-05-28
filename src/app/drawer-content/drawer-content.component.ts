48b3a02d36661d09c78492c84e23fcae842ef7e2
fearles545 <iligol3@gmail.com>
2025-05-28 13:54:17 +0300

    feat: update drawer content component to include phone data input and modify navigation links

--- src/app/drawer-content/drawer-content.component.ts
+++ src/app/drawer-content/drawer-content.component.ts
@@ -1,12 +1,23 @@
-import { Component, output } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { SocialIconData } from '../core/icons.data';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { SocialIconLinkComponent } from '../social-icon-link/social-icon-link.component';
 
 @Component({
   selector: 'app-drawer-content',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    SocialIconLinkComponent,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './drawer-content.component.html',
   styleUrl: './drawer-content.component.css',
 })
 export class DrawerContentComponent {
+  phoneData = input.required<SocialIconData>();
   onLinkClick = output<void>();
 }
