ad6373e337313fced4c10178a181c470c86c06cb
fearles545 <iligol3@gmail.com>
2025-06-05 16:13:23 +0300

    feat: streamline imports in drawer-content component and update document assets

--- src/app/drawer-content/drawer-content.component.ts
+++ src/app/drawer-content/drawer-content.component.ts
@@ -3,17 +3,10 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 import { SocialIconData } from '../core/icons.data';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { SocialIconLinkComponent } from '../social-icon-link/social-icon-link.component';
 
 @Component({
   selector: 'app-drawer-content',
-  imports: [
-    RouterLink,
-    RouterLinkActive,
-    SocialIconLinkComponent,
-    MatButtonModule,
-    MatIconModule,
-  ],
+  imports: [RouterLink, RouterLinkActive, MatButtonModule, MatIconModule],
   templateUrl: './drawer-content.component.html',
   styleUrl: './drawer-content.component.css',
 })
