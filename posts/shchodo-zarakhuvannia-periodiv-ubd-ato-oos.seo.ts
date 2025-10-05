4a8ddb0d2a44f60ec380bc8770536052aad5e0de
Fearles545 <40427151+Fearles545@users.noreply.github.com>
2025-10-05 22:09:21 +0000

    Deploying to gh-pages from @ Fearles545/advocate-website@cd69e8e21871a626a63289a390174b2f6b1aa2a0 🚀

---
+++ posts/shchodo-zarakhuvannia-periodiv-ubd-ato-oos.seo.ts
@@ -0,0 +1,129 @@
+export const shchodo_zarakhuvannia_periodiv_ubd_ato_oos = {
+  'shchodo-zarakhuvannia-periodiv-ubd-ato-oos': {
+    title:
+      'Щодо зарахування періодів УБД, АТО, ООС | Адвокат Поддяча Юлія Юріївна',
+    description:
+      'Пояснюємо, як підтвердити страховий стаж без трудової книжки.',
+    keywords:
+      'УБД, АТО, ООС, зарахування періодів служби, страховий стаж, пільговий стаж, пенсійний фонд, петиція КМУ',
+    type: 'article',
+    publishedTime: '2025-06-30T09:00:00+03:00',
+    modifiedTime: '2025-06-30T09:00:00+03:00',
+    dateCreated: '2025-06-30T09:00:00+03:00',
+    canonical: '/blog/shchodo-zarakhuvannia-periodiv-ubd-ato-oos/',
+    schema: {
+      '@context': 'https://schema.org',
+      '@graph': [
+        {
+          '@type': 'BreadcrumbList',
+          '@id': '/blog/shchodo-zarakhuvannia-periodiv-ubd-ato-oos/#breadcrumb',
+          itemListElement: [
+            {
+              '@type': 'ListItem',
+              position: 1,
+              name: 'Головна',
+              item: 'https://poddiachaiuliia.com.ua/',
+            },
+            {
+              '@type': 'ListItem',
+              position: 2,
+              name: 'Блог',
+              item: 'https://poddiachaiuliia.com.ua/blog/',
+            },
+            {
+              '@type': 'ListItem',
+              position: 3,
+              name: 'Щодо зарахування періодів УБД, АТО, ООС',
+            },
+          ],
+        },
+        {
+          '@type': 'WebPage',
+          '@id': '/blog/shchodo-zarakhuvannia-periodiv-ubd-ato-oos/#webpage',
+          url: 'https://poddiachaiuliia.com.ua/blog/shchodo-zarakhuvannia-periodiv-ubd-ato-oos/',
+          name: 'Щодо зарахування періодів УБД, АТО, ООС',
+          isPartOf: {
+            '@id': '/#website',
+          },
+          primaryImageOfPage: {
+            '@id':
+              '/blog/shchodo-zarakhuvannia-periodiv-ubd-ato-oos/#primaryimage',
+          },
+          datePublished: '2025-06-30T09:00:00+03:00',
+          dateModified: '2025-06-30T09:00:00+03:00',
+          description:
+            'Пояснюємо, як підтвердити страховий стаж без трудової книжки.',
+          breadcrumb: {
+            '@id':
+              '/blog/shchodo-zarakhuvannia-periodiv-ubd-ato-oos/#breadcrumb',
+          },
+          inLanguage: 'uk-UA',
+          potentialAction: [
+            {
+              '@type': 'ReadAction',
+              target: [
+                'https://poddiachaiuliia.com.ua/blog/shchodo-zarakhuvannia-periodiv-ubd-ato-oos/',
+              ],
+            },
+          ],
+        },
+        {
+          '@type': 'ImageObject',
+          '@id':
+            '/blog/shchodo-zarakhuvannia-periodiv-ubd-ato-oos/#primaryimage',
+          inLanguage: 'uk-UA',
+          url: 'https://poddiachaiuliia.com.ua/assets/logo/og-logo.png',
+          contentUrl: 'https://poddiachaiuliia.com.ua/assets/logo/og-logo.png',
+          width: 1200,
+          height: 630,
+        },
+        {
+          '@type': 'BlogPosting',
+          '@id': '/blog/shchodo-zarakhuvannia-periodiv-ubd-ato-oos/#article',
+          headline: 'Щодо зарахування періодів УБД, АТО, ООС',
+          description:
+            'Пояснюємо, як підтвердити страховий стаж без трудової книжки.',
+          keywords: [
+            'УБД',
+            'АТО',
+            'ООС',
+            'зарахування періодів служби',
+            'страховий стаж',
+            'пільговий стаж',
+            'пенсійний фонд',
+            'петиція КМУ',
+          ],
+          articleSection: 'Пенсійне право',
+          inLanguage: 'uk-UA',
+          isPartOf: {
+            '@id': '/blog/shchodo-zarakhuvannia-periodiv-ubd-ato-oos/#webpage',
+          },
+          mainEntityOfPage: {
+            '@id': '/blog/shchodo-zarakhuvannia-periodiv-ubd-ato-oos/#webpage',
+          },
+          author: {
+            '@type': 'Person',
+            '@id': '/#person',
+            name: 'Поддяча Юлія Юріївна',
+            url: 'https://poddiachaiuliia.com.ua/',
+            image: {
+              '@type': 'ImageObject',
+              '@id': '/#personlogo',
+              url: 'https://poddiachaiuliia.com.ua/assets/logo/og-logo.png',
+              caption: 'Поддяча Юлія Юріївна',
+            },
+          },
+          publisher: {
+            '@id': '/#organization',
+          },
+          image: {
+            '@id':
+              '/blog/shchodo-zarakhuvannia-periodiv-ubd-ato-oos/#primaryimage',
+          },
+          datePublished: '2025-06-30T09:00:00+03:00',
+          dateModified: '2025-06-30T09:00:00+03:00',
+        },
+      ],
+    },
+  },
+};
