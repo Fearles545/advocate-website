f3c57877e2376e51714708e7929dfe42d9d0ae2f
Fearles545 <40427151+Fearles545@users.noreply.github.com>
2025-10-22 18:59:23 +0000

    Deploying to gh-pages from @ Fearles545/advocate-website@6acdc0e5668a2e3d3ddc38495e4304122f18a831 🚀

---
+++ posts/pravo-zhinok-yaki-narodyly-ta-vykhovaly-piatokh-ditei-na-dostrokove-pryznachennia-pensii.seo.ts
@@ -0,0 +1,119 @@
+export const pravo_zhinok_yaki_narodyly_ta_vykhovaly_piatokh_ditei_na_dostrokove_pryznachennia_pensii =
+  {
+    'pravo-zhinok-yaki-narodyly-ta-vykhovaly-piatokh-ditei-na-dostrokove-pryznachennia-pensii':
+      {
+        title:
+          'Право жінок, які народили та виховали п’ятьох дітей, на дострокове призначення пенсії | Адвокат Поддяча Юлія Юріївна',
+        description:
+          'Дострокова пенсія матерям, які народили та виховали п’ятьох і більше дітей: умови (50 років, 15 років стажу), зарахування догляду до стажу, перелік документів, надбавка за особливі заслуги та як оскаржити відмову ПФУ.',
+        keywords:
+          'дострокова пенсія матерям, пенсія багатодітним матерям, п’ять дітей пенсія, 50 років 15 років стажу, догляд за дитиною у стаж, документи для призначення пенсії, надбавка за особливі заслуги, оскарження відмови ПФУ, пункт 3 статті 115 Закону 1058-IV',
+        type: 'article',
+        publishedTime: '2025-10-22T09:00:00+03:00',
+        modifiedTime: '2025-10-22T09:00:00+03:00',
+        dateCreated: '2025-04-20T09:00:00+03:00',
+        canonical:
+          '/blog/pravo-zhinok-yaki-narodyly-ta-vykhovaly-piatokh-ditei-na-dostrokove-pryznachennia-pensii/',
+        schema: {
+          '@context': 'https://schema.org',
+          '@graph': [
+            {
+              '@type': 'BreadcrumbList',
+              '@id':
+                'https://www.advocate-pensia.com.ua/blog/pravo-zhinok-yaki-narodyly-ta-vykhovaly-piatokh-ditei-na-dostrokove-pryznachennia-pensii/#breadcrumbs',
+              itemListElement: [
+                {
+                  '@type': 'ListItem',
+                  position: 1,
+                  name: 'Головна',
+                  item: {
+                    '@type': 'WebPage',
+                    '@id': 'https://www.advocate-pensia.com.ua/',
+                    name: 'Головна',
+                  },
+                },
+                {
+                  '@type': 'ListItem',
+                  position: 2,
+                  name: 'Блог',
+                  item: {
+                    '@type': 'WebPage',
+                    '@id': 'https://www.advocate-pensia.com.ua/blog/',
+                    name: 'Блог',
+                  },
+                },
+                {
+                  '@type': 'ListItem',
+                  position: 3,
+                  name: 'Право жінок, які народили та виховали п’ятьох дітей, на дострокове призначення пенсії',
+                  item: {
+                    '@type': 'WebPage',
+                    '@id':
+                      'https://www.advocate-pensia.com.ua/blog/pravo-zhinok-yaki-narodyly-ta-vykhovaly-piatokh-ditei-na-dostrokove-pryznachennia-pensii/',
+                    name: 'Право жінок, які народили та виховали п’ятьох дітей, на дострокове призначення пенсії',
+                  },
+                },
+              ],
+            },
+            {
+              '@type': 'WebPage',
+              '@id':
+                'https://www.advocate-pensia.com.ua/blog/pravo-zhinok-yaki-narodyly-ta-vykhovaly-piatokh-ditei-na-dostrokove-pryznachennia-pensii/#webpage',
+              name: 'Право жінок, які народили та виховали п’ятьох дітей, на дострокове призначення пенсії',
+              url: 'https://www.advocate-pensia.com.ua/blog/pravo-zhinok-yaki-narodyly-ta-vykhovaly-piatokh-ditei-na-dostrokove-pryznachennia-pensii/',
+              inLanguage: 'uk-UA',
+              isPartOf: {
+                '@id': 'https://www.advocate-pensia.com.ua/#website',
+              },
+              breadcrumb: {
+                '@id':
+                  'https://www.advocate-pensia.com.ua/blog/pravo-zhinok-yaki-narodyly-ta-vykhovaly-piatokh-ditei-na-dostrokove-pryznachennia-pensii/#breadcrumbs',
+              },
+            },
+            {
+              '@type': 'BlogPosting',
+              '@id':
+                'https://www.advocate-pensia.com.ua/blog/pravo-zhinok-yaki-narodyly-ta-vykhovaly-piatokh-ditei-na-dostrokove-pryznachennia-pensii/#article',
+              headline:
+                'Право жінок, які народили та виховали п’ятьох дітей, на дострокове призначення пенсії',
+              description:
+                'Право жінок, які народили та виховали п’ятьох дітей, на дострокову пенсію: умови призначення, зарахування догляду до стажу, документи та оскарження відмови ПФУ.',
+              datePublished: '2025-10-22T09:00:00+03:00',
+              dateModified: '2025-10-22T09:00:00+03:00',
+              dateCreated: '2025-04-20T09:00:00+03:00',
+              author: {
+                '@type': 'Person',
+                name: 'Поддяча Юлія Юріївна',
+                url: 'https://www.advocate-pensia.com.ua/about-me/',
+              },
+              publisher: { '@id': 'https://www.advocate-pensia.com.ua/#org' },
+              image: [
+                'https://www.advocate-pensia.com.ua/assets/images/og/default-og-image.webp',
+              ],
+              inLanguage: 'uk-UA',
+              isPartOf: {
+                '@id': 'https://www.advocate-pensia.com.ua/#website',
+              },
+              keywords: [
+                'дострокова пенсія матерям',
+                'пенсія багатодітним матерям',
+                'п’ять дітей пенсія',
+                '50 років',
+                '15 років страхового стажу',
+                'догляд за дитиною у стаж',
+                'документи для призначення пенсії',
+                'надбавка за особливі заслуги',
+                'оскарження відмови ПФУ',
+                'пункт 3 статті 115 Закону 1058-IV',
+              ],
+              articleSection: 'Пенсійне право',
+              mainEntityOfPage: {
+                '@id':
+                  'https://www.advocate-pensia.com.ua/blog/pravo-zhinok-yaki-narodyly-ta-vykhovaly-piatokh-ditei-na-dostrokove-pryznachennia-pensii/#webpage',
+              },
+              url: 'https://www.advocate-pensia.com.ua/blog/pravo-zhinok-yaki-narodyly-ta-vykhovaly-piatokh-ditei-na-dostrokove-pryznachennia-pensii/',
+            },
+          ],
+        },
+      },
+  };
