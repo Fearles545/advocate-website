97066b21d16318a12d8d11c26f7d922e1ca54975
Fearles545 <40427151+Fearles545@users.noreply.github.com>
2025-10-25 14:44:48 +0000

    Deploying to gh-pages from @ Fearles545/advocate-website@610ec22ffabddb16469b99aee567dac741487a92 🚀

---
+++ posts/nepravylna-indeksatsiia-pensii-pryznachenykh-z-2020-roku-shcho-zminyla-postanova-velykoi-palaty-vs-i-iak-diati-pensioneru.seo.ts
@@ -0,0 +1,118 @@
+export const nepravylna_indeksatsiia_pensii_pryznachenykh_z_2020_roku_shcho_zminyla_postanova_velykoi_palaty_vs_i_iak_diati_pensioneru =
+  {
+    'nepravylna-indeksatsiia-pensii-pryznachenykh-z-2020-roku-shcho-zminyla-postanova-velykoi-palaty-vs-i-iak-diati-pensioneru':
+      {
+        title:
+          'Неправильна індексація пенсій, призначених з 2020 року: що змінила постанова Великої Палати ВС і як діяти пенсіонеру | Адвокат Поддяча Юлія Юріївна',
+        description:
+          'Велика Палата ВС підтвердила: пенсії, призначені після 2020 року, треба індексувати коефіцієнтами, а не фіксованими 100–135 грн; пояснюємо дії пенсіонера.',
+        keywords:
+          'індексація пенсій після 2020 року, Велика Палата Верховного Суду, перерахунок пенсій, коефіцієнт індексації, фіксована доплата, помилка ПФУ, як оскаржити відмову, пенсійне право, адвокат по пенсіях',
+        type: 'article',
+        publishedTime: '2025-10-25T09:00:00+03:00',
+        modifiedTime: '2025-10-25T09:00:00+03:00',
+        dateCreated: '2025-04-25T09:00:00+03:00',
+        canonical:
+          '/blog/nepravylna-indeksatsiia-pensii-pryznachenykh-z-2020-roku-shcho-zminyla-postanova-velykoi-palaty-vs-i-iak-diati-pensioneru/',
+        schema: {
+          '@context': 'https://schema.org',
+          '@graph': [
+            {
+              '@type': 'BreadcrumbList',
+              '@id':
+                'https://www.advocate-pensia.com.ua/blog/nepravylna-indeksatsiia-pensii-pryznachenykh-z-2020-roku-shcho-zminyla-postanova-velykoi-palaty-vs-i-iak-diati-pensioneru/#breadcrumbs',
+              itemListElement: [
+                {
+                  '@type': 'ListItem',
+                  position: 1,
+                  name: 'Головна',
+                  item: {
+                    '@type': 'WebPage',
+                    '@id': 'https://www.advocate-pensia.com.ua/',
+                    name: 'Головна',
+                  },
+                },
+                {
+                  '@type': 'ListItem',
+                  position: 2,
+                  name: 'Блог',
+                  item: {
+                    '@type': 'WebPage',
+                    '@id': 'https://www.advocate-pensia.com.ua/blog/',
+                    name: 'Блог',
+                  },
+                },
+                {
+                  '@type': 'ListItem',
+                  position: 3,
+                  name: 'Як Велика Палата ВС змінила правила індексації пенсій після 2020 року',
+                  item: {
+                    '@type': 'WebPage',
+                    '@id':
+                      'https://www.advocate-pensia.com.ua/blog/nepravylna-indeksatsiia-pensii-pryznachenykh-z-2020-roku-shcho-zminyla-postanova-velykoi-palaty-vs-i-iak-diati-pensioneru/',
+                    name: 'Як Велика Палата ВС змінила правила індексації пенсій після 2020 року',
+                  },
+                },
+              ],
+            },
+            {
+              '@type': 'WebPage',
+              '@id':
+                'https://www.advocate-pensia.com.ua/blog/nepravylna-indeksatsiia-pensii-pryznachenykh-z-2020-roku-shcho-zminyla-postanova-velykoi-palaty-vs-i-iak-diati-pensioneru/#webpage',
+              name: 'Як Велика Палата ВС змінила правила індексації пенсій після 2020 року',
+              url: 'https://www.advocate-pensia.com.ua/blog/nepravylna-indeksatsiia-pensii-pryznachenykh-z-2020-roku-shcho-zminyla-postanova-velykoi-palaty-vs-i-iak-diati-pensioneru/',
+              inLanguage: 'uk-UA',
+              isPartOf: {
+                '@id': 'https://www.advocate-pensia.com.ua/#website',
+              },
+              breadcrumb: {
+                '@id':
+                  'https://www.advocate-pensia.com.ua/blog/nepravylna-indeksatsiia-pensii-pryznachenykh-z-2020-roku-shcho-zminyla-postanova-velykoi-palaty-vs-i-iak-diati-pensioneru/#breadcrumbs',
+              },
+            },
+            {
+              '@type': 'BlogPosting',
+              '@id':
+                'https://www.advocate-pensia.com.ua/blog/nepravylna-indeksatsiia-pensii-pryznachenykh-z-2020-roku-shcho-zminyla-postanova-velykoi-palaty-vs-i-iak-diati-pensioneru/#article',
+              headline:
+                'Як Велика Палата ВС змінила правила індексації пенсій після 2020 року | Перерахунок і дії пенсіонера',
+              description:
+                'Постанова Великої Палати Верховного Суду визначила: індексацію пенсій потрібно проводити за коефіцієнтами, а не фіксованими доплатами. Дізнайтесь, як перевірити правильність розрахунку та що робити пенсіонеру.',
+              datePublished: '2025-10-25T09:00:00+03:00',
+              dateModified: '2025-10-25T09:00:00+03:00',
+              dateCreated: '2025-04-25T09:00:00+03:00',
+              author: {
+                '@type': 'Person',
+                name: 'Поддяча Юлія Юріївна',
+                url: 'https://www.advocate-pensia.com.ua/about-me/',
+              },
+              publisher: { '@id': 'https://www.advocate-pensia.com.ua/#org' },
+              image: [
+                'https://www.advocate-pensia.com.ua/assets/images/og/default-og-image.webp',
+              ],
+              inLanguage: 'uk-UA',
+              isPartOf: {
+                '@id': 'https://www.advocate-pensia.com.ua/#website',
+              },
+              keywords: [
+                'індексація пенсій після 2020 року',
+                'постанова Великої Палати Верховного Суду',
+                'перерахунок пенсії',
+                'коефіцієнти індексації',
+                'фіксована доплата',
+                'помилка ПФУ',
+                'оскарження відмови',
+                'пенсійне законодавство',
+                'адвокат по пенсіях',
+              ],
+              articleSection: 'Пенсійне право',
+              mainEntityOfPage: {
+                '@id':
+                  'https://www.advocate-pensia.com.ua/blog/nepravylna-indeksatsiia-pensii-pryznachenykh-z-2020-roku-shcho-zminyla-postanova-velykoi-palaty-vs-i-iak-diati-pensioneru/#webpage',
+              },
+              url: 'https://www.advocate-pensia.com.ua/blog/nepravylna-indeksatsiia-pensii-pryznachenykh-z-2020-roku-shcho-zminyla-postanova-velykoi-palaty-vs-i-iak-diati-pensioneru/',
+            },
+          ],
+        },
+      },
+  };
