7e9a66037aaac68eec9c236b412e2cf877f7aad1
Fearles545 <40427151+Fearles545@users.noreply.github.com>
2025-11-09 13:25:24 +0000

    Deploying to gh-pages from @ Fearles545/advocate-website@9d7a1fa36706459d5d193a9d08254c0625117700 🚀

---
+++ posts/neobkhidnyi-strakhovyi-stazh-dlia-pryznachennia-pensii-za-vikom.seo.ts
@@ -0,0 +1,115 @@
+export const neobkhidnyi_strakhovyi_stazh_dlia_pryznachennia_pensii_za_vikom = {
+  'neobkhidnyi-strakhovyi-stazh-dlia-pryznachennia-pensii-za-vikom': {
+    title:
+      'Необхідний страховий стаж для пенсії за віком | Адвокат Поддяча Юлія Юріївна',
+    description:
+      'Вимоги до страхового стажу для пенсії за віком у 2025-2028 роках: 32-35 років для виходу у 60, 22-25 років для 63 років, 15 років для 65. З 2028 року — пенсія незалежно від віку при 40 роках стажу.',
+    keywords:
+      'страховий стаж для пенсії, необхідний стаж 2025, мінімальний стаж для пенсії, скільки стажу потрібно для пенсії, перевірка страхового стажу, пенсія в 60 років стаж, пенсія 40 років стажу, вимоги до стажу',
+    type: 'article',
+    publishedTime: '2025-11-09T09:00:00+02:00',
+    modifiedTime: '2025-11-09T09:00:00+02:00',
+    dateCreated: '2025-04-29T09:00:00+02:00',
+    canonical:
+      '/blog/neobkhidnyi-strakhovyi-stazh-dlia-pryznachennia-pensii-za-vikom/',
+    schema: {
+      '@context': 'https://schema.org',
+      '@graph': [
+        {
+          '@type': 'BreadcrumbList',
+          '@id':
+            'https://www.advocate-pensia.com.ua/blog/neobkhidnyi-strakhovyi-stazh-dlia-pryznachennia-pensii-za-vikom/#breadcrumbs',
+          itemListElement: [
+            {
+              '@type': 'ListItem',
+              position: 1,
+              name: 'Головна',
+              item: {
+                '@type': 'WebPage',
+                '@id': 'https://www.advocate-pensia.com.ua/',
+                name: 'Головна',
+              },
+            },
+            {
+              '@type': 'ListItem',
+              position: 2,
+              name: 'Блог',
+              item: {
+                '@type': 'WebPage',
+                '@id': 'https://www.advocate-pensia.com.ua/blog/',
+                name: 'Блог',
+              },
+            },
+            {
+              '@type': 'ListItem',
+              position: 3,
+              name: 'Необхідний страховий стаж для призначення пенсії за віком',
+              item: {
+                '@type': 'WebPage',
+                '@id':
+                  'https://www.advocate-pensia.com.ua/blog/neobkhidnyi-strakhovyi-stazh-dlia-pryznachennia-pensii-za-vikom/',
+                name: 'Необхідний страховий стаж для призначення пенсії за віком',
+              },
+            },
+          ],
+        },
+        {
+          '@type': 'WebPage',
+          '@id':
+            'https://www.advocate-pensia.com.ua/blog/neobkhidnyi-strakhovyi-stazh-dlia-pryznachennia-pensii-za-vikom/#webpage',
+          name: 'Необхідний страховий стаж для призначення пенсії за віком',
+          url: 'https://www.advocate-pensia.com.ua/blog/neobkhidnyi-strakhovyi-stazh-dlia-pryznachennia-pensii-za-vikom/',
+          inLanguage: 'uk-UA',
+          isPartOf: {
+            '@id': 'https://www.advocate-pensia.com.ua/#website',
+          },
+          breadcrumb: {
+            '@id':
+              'https://www.advocate-pensia.com.ua/blog/neobkhidnyi-strakhovyi-stazh-dlia-pryznachennia-pensii-za-vikom/#breadcrumbs',
+          },
+        },
+        {
+          '@type': 'BlogPosting',
+          '@id':
+            'https://www.advocate-pensia.com.ua/blog/neobkhidnyi-strakhovyi-stazh-dlia-pryznachennia-pensii-za-vikom/#article',
+          headline: 'Необхідний страховий стаж для призначення пенсії за віком',
+          description:
+            'Вимоги до страхового стажу для пенсії за віком у 2025-2028 роках: 32-35 років для виходу у 60, 22-25 років для 63 років, 15 років для 65. З 2028 року — пенсія незалежно від віку при 40 роках стажу.',
+          datePublished: '2025-11-09T09:00:00+02:00',
+          dateModified: '2025-11-09T09:00:00+02:00',
+          dateCreated: '2025-04-29T09:00:00+02:00',
+          author: {
+            '@type': 'Person',
+            name: 'Поддяча Юлія Юріївна',
+            url: 'https://www.advocate-pensia.com.ua/about-me/',
+          },
+          publisher: {
+            '@id': 'https://www.advocate-pensia.com.ua/#org',
+          },
+          image: [
+            'https://www.advocate-pensia.com.ua/assets/images/og/default-og-image.webp',
+          ],
+          inLanguage: 'uk-UA',
+          isPartOf: {
+            '@id': 'https://www.advocate-pensia.com.ua/#website',
+          },
+          keywords: [
+            'страховий стаж для пенсії',
+            'необхідний стаж 2025',
+            'мінімальний стаж для пенсії',
+            'скільки стажу потрібно для пенсії',
+            'перевірка страхового стажу',
+            'пенсія в 60 років стаж',
+            'пенсія 40 років стажу',
+            'вимоги до стажу',
+          ],
+          articleSection: 'Пенсійне право',
+          mainEntityOfPage: {
+            '@id':
+              'https://www.advocate-pensia.com.ua/blog/neobkhidnyi-strakhovyi-stazh-dlia-pryznachennia-pensii-za-vikom/#webpage',
+          },
+        },
+      ],
+    },
+  },
+};
