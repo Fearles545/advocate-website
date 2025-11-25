a3a965b43f1f2f00fdd7f0ed5418f993e306f01c
Fearles545 <40427151+Fearles545@users.noreply.github.com>
2025-11-25 17:05:32 +0000

    Deploying to gh-pages from @ Fearles545/advocate-website@8fb06dd18861218348be9cb76cc6379c03e55884 🚀

---
+++ posts/koly-mozhna-podaty-zaiavu-na-pryznachennia-pensii-za-vikom-ta-iakyi-strok-dlia-podannia-dodatkovykh-dokumentiv.seo.ts
@@ -0,0 +1,118 @@
+export const koly_mozhna_podaty_zaiavu_na_pryznachennia_pensii_za_vikom_ta_iakyi_strok_dlia_podannia_dodatkovykh_dokumentiv =
+  {
+    'koly-mozhna-podaty-zaiavu-na-pryznachennia-pensii-za-vikom-ta-iakyi-strok-dlia-podannia-dodatkovykh-dokumentiv':
+      {
+        title:
+          'Коли можна подати заяву на призначення пенсії за віком | Адвокат Поддяча Юлія Юріївна',
+        description:
+          'Пенсію призначають з дня після досягнення пенсійного віку, якщо звернутися протягом 3 місяців. Можна подати заяву за місяць до народження. На донесення документів — 3 місяці без втрати дати звернення.',
+        keywords:
+          'коли подавати заяву на пенсію, строк звернення за пенсією, три місяці після пенсійного віку, додаткові документи пенсія, стаття 45 Закон 1058, дата звернення за пенсією, втрата пенсії строки, донесення документів ПФУ',
+        type: 'article',
+        publishedTime: '2025-11-25T09:00:00+02:00',
+        modifiedTime: '2025-11-25T09:00:00+02:00',
+        dateCreated: '2025-11-25T09:00:00+02:00',
+        canonical:
+          '/blog/koly-mozhna-podaty-zaiavu-na-pryznachennia-pensii-za-vikom-ta-iakyi-strok-dlia-podannia-dodatkovykh-dokumentiv/',
+        schema: {
+          '@context': 'https://schema.org',
+          '@graph': [
+            {
+              '@type': 'BreadcrumbList',
+              '@id':
+                'https://www.advocate-pensia.com.ua/blog/koly-mozhna-podaty-zaiavu-na-pryznachennia-pensii-za-vikom-ta-iakyi-strok-dlia-podannia-dodatkovykh-dokumentiv/#breadcrumbs',
+              itemListElement: [
+                {
+                  '@type': 'ListItem',
+                  position: 1,
+                  name: 'Головна',
+                  item: {
+                    '@type': 'WebPage',
+                    '@id': 'https://www.advocate-pensia.com.ua/',
+                    name: 'Головна',
+                  },
+                },
+                {
+                  '@type': 'ListItem',
+                  position: 2,
+                  name: 'Блог',
+                  item: {
+                    '@type': 'WebPage',
+                    '@id': 'https://www.advocate-pensia.com.ua/blog/',
+                    name: 'Блог',
+                  },
+                },
+                {
+                  '@type': 'ListItem',
+                  position: 3,
+                  name: 'Коли можна подати заяву на призначення пенсії за віком та який строк для подання додаткових документів',
+                  item: {
+                    '@type': 'WebPage',
+                    '@id':
+                      'https://www.advocate-pensia.com.ua/blog/koly-mozhna-podaty-zaiavu-na-pryznachennia-pensii-za-vikom-ta-iakyi-strok-dlia-podannia-dodatkovykh-dokumentiv/',
+                    name: 'Коли можна подати заяву на призначення пенсії за віком та який строк для подання додаткових документів',
+                  },
+                },
+              ],
+            },
+            {
+              '@type': 'WebPage',
+              '@id':
+                'https://www.advocate-pensia.com.ua/blog/koly-mozhna-podaty-zaiavu-na-pryznachennia-pensii-za-vikom-ta-iakyi-strok-dlia-podannia-dodatkovykh-dokumentiv/#webpage',
+              name: 'Коли можна подати заяву на призначення пенсії за віком та який строк для подання додаткових документів',
+              url: 'https://www.advocate-pensia.com.ua/blog/koly-mozhna-podaty-zaiavu-na-pryznachennia-pensii-za-vikom-ta-iakyi-strok-dlia-podannia-dodatkovykh-dokumentiv/',
+              inLanguage: 'uk-UA',
+              isPartOf: {
+                '@id': 'https://www.advocate-pensia.com.ua/#website',
+              },
+              breadcrumb: {
+                '@id':
+                  'https://www.advocate-pensia.com.ua/blog/koly-mozhna-podaty-zaiavu-na-pryznachennia-pensii-za-vikom-ta-iakyi-strok-dlia-podannia-dodatkovykh-dokumentiv/#breadcrumbs',
+              },
+            },
+            {
+              '@type': 'BlogPosting',
+              '@id':
+                'https://www.advocate-pensia.com.ua/blog/koly-mozhna-podaty-zaiavu-na-pryznachennia-pensii-za-vikom-ta-iakyi-strok-dlia-podannia-dodatkovykh-dokumentiv/#article',
+              headline:
+                'Коли можна подати заяву на призначення пенсії за віком та який строк для подання додаткових документів',
+              description:
+                'Пенсію призначають з дня після досягнення пенсійного віку, якщо звернутися протягом 3 місяців. Можна подати заяву за місяць до народження. На донесення документів — 3 місяці без втрати дати звернення.',
+              datePublished: '2025-11-25T09:00:00+02:00',
+              dateModified: '2025-11-25T09:00:00+02:00',
+              dateCreated: '2025-11-25T09:00:00+02:00',
+              author: {
+                '@type': 'Person',
+                name: 'Поддяча Юлія Юріївна',
+                url: 'https://www.advocate-pensia.com.ua/about-me/',
+              },
+              publisher: {
+                '@id': 'https://www.advocate-pensia.com.ua/#org',
+              },
+              image: [
+                'https://www.advocate-pensia.com.ua/assets/images/og/default-og-image.webp',
+              ],
+              inLanguage: 'uk-UA',
+              isPartOf: {
+                '@id': 'https://www.advocate-pensia.com.ua/#website',
+              },
+              keywords: [
+                'коли подавати заяву на пенсію',
+                'строк звернення за пенсією',
+                'три місяці після пенсійного віку',
+                'додаткові документи пенсія',
+                'стаття 45 Закон 1058',
+                'дата звернення за пенсією',
+                'втрата пенсії строки',
+                'донесення документів ПФУ',
+              ],
+              articleSection: 'Пенсійне право',
+              mainEntityOfPage: {
+                '@id':
+                  'https://www.advocate-pensia.com.ua/blog/koly-mozhna-podaty-zaiavu-na-pryznachennia-pensii-za-vikom-ta-iakyi-strok-dlia-podannia-dodatkovykh-dokumentiv/#webpage',
+              },
+            },
+          ],
+        },
+      },
+  };
