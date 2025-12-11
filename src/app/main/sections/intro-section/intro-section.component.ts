d7a07659d831f7e4f30501d421ada272dec55cde
fearles545 <iligol3@gmail.com>
2025-12-11 23:52:48 +0200

    feat(intro-section): enhance intro section styling and image handling
    * Add aspect ratio and gradient background to photo container
    * Update main photo attributes for better loading and display
    * Import NgOptimizedImage for improved image optimization
    * Replace main image with a new optimized version

--- src/app/main/sections/intro-section/intro-section.component.ts
+++ src/app/main/sections/intro-section/intro-section.component.ts
@@ -2,10 +2,11 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-intro-section',
-  imports: [MatButtonModule, MatIcon, RouterLink],
+  imports: [MatButtonModule, MatIcon, RouterLink, NgOptimizedImage],
   templateUrl: './intro-section.component.html',
   styleUrl: './intro-section.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
