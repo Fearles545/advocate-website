c5b9b8bff272dd1fd3af468bc9295e30861d05c5
fearles545 <iligol3@gmail.com>
2025-12-31 21:27:59 +0200

    feat(feedbacks): replace Google reviews button with CTA component
    - Removed custom styles for the Google reviews button.
    - Integrated CtaOutlineButtonComponent for better consistency.
    - Updated icon and styles for improved UI.

--- src/app/feedbacks/feedbacks.component.ts
+++ src/app/feedbacks/feedbacks.component.ts
@@ -1,13 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { feedbacks, Feedback } from './feedbacks.data';
+import { CtaOutlineButtonComponent } from '@shared/components/cta-outline-button';
 
 @Component({
   selector: 'app-feedbacks',
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './feedbacks.component.html',
   styleUrl: './feedbacks.component.css',
-  imports: [MatIcon],
+  imports: [MatIcon, CtaOutlineButtonComponent],
 })
 export class FeedbacksComponent {
   feedbacks: Feedback[] = feedbacks;
