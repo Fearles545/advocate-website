2c99635a52d8f91eb67aadf3a3134268a705b7d7
Fearles545 <40427151+Fearles545@users.noreply.github.com>
2026-04-02 18:05:51 +0000

    Deploying to gh-pages from @ Fearles545/advocate-website@be3e67313934116290ca8043cbf2ee9682185bdd 🚀

---
+++ posts/holosuvannia-proty-proiektu-kmu-821.seo.ts
@@ -0,0 +1,114 @@
+export const holosuvannia_proty_proiektu_kmu_821 = {
+  'holosuvannia-proty-proiektu-kmu-821': {
+    title:
+      'Голосування проти проєкту на заміну постанови КМУ №821 | Адвокат Поддяча Юлія Юріївна',
+    description:
+      'Суд визнав постанову КМУ №821 протиправною, але Мінсоцполітики виставило аналогічний проєкт. Інструкція як проголосувати проти на сайті msp.gov.ua через BankID.',
+    keywords:
+      'постанова КМУ 821, голосування проти, Мінсоцполітики, пенсія за рішенням суду, Пенсійний фонд, громадське обговорення',
+    type: 'article',
+    publishedTime: '2026-04-02T16:34:00+03:00',
+    modifiedTime: '2026-04-02T16:34:00+03:00',
+    dateCreated: '2026-04-02T16:34:00+03:00',
+    canonical: '/blog/holosuvannia-proty-proiektu-kmu-821/',
+    schema: {
+      '@context': 'https://schema.org',
+      '@graph': [
+        {
+          '@type': 'BreadcrumbList',
+          '@id':
+            'https://www.advocate-pensia.com.ua/blog/holosuvannia-proty-proiektu-kmu-821/#breadcrumbs',
+          itemListElement: [
+            {
+              '@type': 'ListItem',
+              position: 1,
+              name: 'Головна',
+              item: {
+                '@type': 'WebPage',
+                '@id': 'https://www.advocate-pensia.com.ua/',
+                name: 'Головна',
+              },
+            },
+            {
+              '@type': 'ListItem',
+              position: 2,
+              name: 'Блог',
+              item: {
+                '@type': 'WebPage',
+                '@id': 'https://www.advocate-pensia.com.ua/blog/',
+                name: 'Блог',
+              },
+            },
+            {
+              '@type': 'ListItem',
+              position: 3,
+              name: 'Голосування проти проєкту на заміну постанови КМУ №821',
+              item: {
+                '@type': 'WebPage',
+                '@id':
+                  'https://www.advocate-pensia.com.ua/blog/holosuvannia-proty-proiektu-kmu-821/',
+                name: 'Голосування проти проєкту на заміну постанови КМУ №821',
+              },
+            },
+          ],
+        },
+        {
+          '@type': 'WebPage',
+          '@id':
+            'https://www.advocate-pensia.com.ua/blog/holosuvannia-proty-proiektu-kmu-821/#webpage',
+          name: 'Голосування проти проєкту на заміну постанови КМУ №821 | Адвокат Поддяча Юлія Юріївна',
+          url: 'https://www.advocate-pensia.com.ua/blog/holosuvannia-proty-proiektu-kmu-821/',
+          inLanguage: 'uk-UA',
+          isPartOf: {
+            '@id': 'https://www.advocate-pensia.com.ua/#website',
+          },
+          breadcrumb: {
+            '@id':
+              'https://www.advocate-pensia.com.ua/blog/holosuvannia-proty-proiektu-kmu-821/#breadcrumbs',
+          },
+        },
+        {
+          '@type': 'BlogPosting',
+          '@id':
+            'https://www.advocate-pensia.com.ua/blog/holosuvannia-proty-proiektu-kmu-821/#article',
+          headline:
+            'Голосування проти проєкту на заміну постанови КМУ №821',
+          description:
+            'Суд визнав постанову КМУ №821 протиправною, але Мінсоцполітики виставило аналогічний проєкт на громадське обговорення. Покрокова інструкція як проголосувати проти на сайті msp.gov.ua через BankID.',
+          datePublished: '2026-04-02T16:34:00+03:00',
+          dateModified: '2026-04-02T16:34:00+03:00',
+          dateCreated: '2026-04-02T16:34:00+03:00',
+          author: {
+            '@type': 'Person',
+            name: 'Поддяча Юлія Юріївна',
+            url: 'https://www.advocate-pensia.com.ua/about-me/',
+          },
+          publisher: {
+            '@id': 'https://www.advocate-pensia.com.ua/#org',
+          },
+          image: [
+            'https://www.advocate-pensia.com.ua/assets/images/og/default-og-image.webp',
+          ],
+          inLanguage: 'uk-UA',
+          isPartOf: {
+            '@id': 'https://www.advocate-pensia.com.ua/#website',
+          },
+          keywords: [
+            'постанова КМУ 821',
+            'голосування проти',
+            'Мінсоцполітики',
+            'пенсія за рішенням суду',
+            'Пенсійний фонд',
+            'громадське обговорення',
+          ],
+          articleSection: 'Пенсійне право',
+          mainEntityOfPage: {
+            '@id':
+              'https://www.advocate-pensia.com.ua/blog/holosuvannia-proty-proiektu-kmu-821/#webpage',
+          },
+          url: 'https://www.advocate-pensia.com.ua/blog/holosuvannia-proty-proiektu-kmu-821/',
+        },
+      ],
+    },
+  },
+};
