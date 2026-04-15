b544dc52a7f2f6f27b832529474bbc9979c384d2
fearles545 <iligol3@gmail.com>
2026-04-15 13:40:11 +0300

    refactor(feedbacks): simplify feedbacks initialization and remove unused imports

--- src/app/feedbacks/feedbacks.component.ts
+++ src/app/feedbacks/feedbacks.component.ts
@@ -1,15 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { feedbacks, Feedback } from './feedbacks.data';
-import { CtaOutlineButtonComponent } from '@shared/components/cta-outline-button';
+// import { CtaOutlineButtonComponent } from '@shared/components/cta-outline-button';
 
 @Component({
   selector: 'app-feedbacks',
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './feedbacks.component.html',
   styleUrl: './feedbacks.component.css',
-  imports: [MatIcon, CtaOutlineButtonComponent],
+  imports: [MatIcon],
 })
 export class FeedbacksComponent {
-  feedbacks: Feedback[] = feedbacks;
+  feedbacks: Feedback[] = [...feedbacks].reverse();
 }
